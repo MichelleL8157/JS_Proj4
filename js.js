73e8f74433693d49d6a7fee75489d55cd408c6a7
MichelleL8157 <98603675+MichelleL8157@users.noreply.github.com>
2023-01-09 04:44:33 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,8 +1,16 @@
-$('.up,.down').click(function () {
-    let item = $(this).parents('tr:first');
-    if ($(this).is('.up')) {
-        item.insertBefore(item.prev());
-    } else {
-        item.insertAfter(item.next());
-    }
-});
+var upBut = document.querySelectorAll(".up");
+var downBut = document.querySelectorAll(".down");
+for (var i = 0; i < upBut.length; i++) {
+    upBut[i].addEventListener('click', function () {
+        var total = this.parentElement;
+        if (total.previousElementSibling)
+        total.parentNode.insertBefore(total, total.previousElementSibling);
+    });
+}
+for (var i = 0; i < downBut.length; i++) {
+    downBut[i].addEventListener('click', function () {
+        var total = this.parentElement;
+        if (total.nextElementSibling)
+        total.parentNode.insertBefore(total.nextElementSibling, total);
+    });
+}
