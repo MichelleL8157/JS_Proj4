6e3eff9e0d3b1b44c2b89f9aa26a09b76f0d9bdb
MichelleL8157 <98603675+MichelleL8157@users.noreply.github.com>
2023-01-09 05:22:08 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,16 +1,36 @@
-var upBut = document.querySelectorAll(".up");
-var downBut = document.querySelectorAll(".down");
-for (var i = 0; i < upBut.length; i++) {
-    upBut[i].addEventListener('click', function () {
-        var total = this.parentElement;
-        if (total.previousElementSibling)
-        total.parentNode.insertBefore(total, total.previousElementSibling);
-    });
+var index; 
+function getSelectedRow()
+{
+    var table = document.getElementById("table");
+    for(var i = 1; i < table.rows.length; i++)
+    {
+        table.rows[i].onclick = function()
+        {
+            if(typeof index !== "undefined"){
+                table.rows[index].classList.toggle("selected");
+            }
+            index = this.rowIndex;
+            this.classList.toggle("selected");
+        };
+    }
 }
-for (var i = 0; i < downBut.length; i++) {
-    downBut[i].addEventListener('click', function () {
-        var total = this.parentElement;
-        if (total.nextElementSibling)
-        total.parentNode.insertBefore(total.nextElementSibling, total);
-    });
+getSelectedRow();
+function upNdown(direction)
+{
+    var rows = document.getElementById("table").rows,
+        parent = rows[index].parentNode;
+        if(direction === "up")
+        {
+            if(index > 1){
+                parent.insertBefore(rows[index],rows[index - 1]);
+                index--;
+            }
+        }
+        if(direction === "down")
+        {
+            if(index < rows.length -1){
+                parent.insertBefore(rows[index + 1],rows[index]);
+                index++;
+            }
+        }
 }
