e5b870b3c6146ce302a9c2fd46da61be8cb454a1
MichelleL8157 <98603675+MichelleL8157@users.noreply.github.com>
2023-01-16 23:47:50 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,68 +1,36 @@
-var index; 
-function getRow() {
+let nameArr = ["Milk Carton", "Oreos", "Whipped Cream", "Cherios"];
+let priceArr = ["6.00", "4.50", "4.00", "2.50"];
+function addRow() {
+    var newItem = document.getElementById("newItem").value;
+    var newPrice = document.getElementById("newPrice").value;
+    nameArr.push(newItem);
+    priceArr.push(newPrice);
     var table = document.getElementById("table");
-    for (var i = 1; i < table.rows.length; i++) {
-        table.rows[i].onclick = function()
-        {
-            if (typeof index !== "undefined") {
-                table.rows[index].classList.toggle("selected");
-            }
-            index = this.rowIndex;
-            this.classList.toggle("selected");
-        };
-    }
-}
-getRow();
-function change(direction) {
-    var rows = document.getElementById("table").rows,
-    parent = rows[index].parentNode;
-    if(direction === "up") {
-        if (index > 1) {
-            parent.insertBefore(rows[index],rows[index - 1]);
-            index--;
-        }
-    }
-    if (direction === "down") {
-        if (index < rows.length -1) {
-            parent.insertBefore(rows[index + 1],rows[index]);
-            index++;
-        }
-    }
-}
-function editRow(num) {
-    document.getElementById("editBut" + num).style.display = "none";
-    document.getElementById("saveBut" + num).style.display = "block";
-        
-    var item = document.getElementById("itemRow" + num);
-    var price = document.getElementById("priceRow" + num);
-        
-    var itemVal = item.innerHTML;
-    var priceVal = price.innerHTML;
-        
-    item.innerHTML = "<input type='text' id='itemText" + num + "' value='" + itemVal + "'>";
-    country.innerHTML = "<input type='text' id=priceText" + num + "' value='" + priceVal + "'>";
-}
-function saveRow(num) {
-    var itemVal2=document.getElementById("itemText" + num).value;
-    var priceVal2=document.getElementById("priceText" + num).value;
-
-    document.getElementById("itemRow" + num).innerHTML = itemVal2;
-    document.getElementById("priceRow" + num).innerHTML = priceVal2;
-
-    document.getElementById("editBut" + num).style.display = "block";
-    document.getElementById("saveBut" + num).style.display = "none";
+    var tableLen = (table.rows.length);
+    var row = table.insertRow(tableLen).outerHTML="<tr id='row" + tableLen + "'><td id='itemRow" + tableLen + "'>" + newItem + "</td><td id='priceRow" + tableLen + "'>" + newPrice + "</td><td><input type='button' value='Delete' class='delete' onclick='deleteRow(" + tableLen + ")'></td></tr>";
+    document.getElementById("newItem").value="";
+    document.getElementById("newPrice").value="";
+    //to review the new list check the console:
+    console.log(nameArr);
+    console.log(priceArr);
 }
 function deleteRow(num) {
     document.getElementById("row" + num + "").outerHTML="";
+    nameArr.splice(num - 1, 1);
+    priceArr.splice(num - 1, 1);
+    //to review the new list check the console:
+    console.log(nameArr);
+    console.log(priceArr);
 }
-function addRow() {
-    var newItem=document.getElementById("newItem").value;
-    var newPrice=document.getElementById("newPrice").value;
-        
-    var table=document.getElementById("table");
-    var tableLen=(table.rows.length) - 1;
-    var row = table.insertRow(tableLen).outerHTML="<tr id='row"+tableLen+"'><td id='itemRow"+tableLen+"'>"+newItem+"</td><td id='priceRow"+tableLen+"'>"+newPrice+"</td><td><input type='button' id='editBut"+tableLen+"' value='Edit' class='edit' onclick='editRow("+tableLen+")'> <input type='button' id='saveBut"+tableLen+"' value='Save' class='save' onclick='saveRow("+tableLen+")'> <input type='button' value='Delete' class='delete' onclick='deleteRow("+tableLen+")'></td></tr>";
-
-    document.getElementById("newItem").value="";
-    document.getElementById("newPrice").value="";
+function change(direction) {
+    let index = document.getElementById("switchRow").value;
+    var rows = document.getElementById("table").rows,
+    parent = rows[index].parentNode;
+    if (direction === "up" && index > 1) {
+        parent.insertBefore(rows[index], rows[index - 1]);
+    }
+    if (direction === "down" && index < rows.length - 1) {
+        index++;
+        parent.insertBefore(rows[index], rows[index - 1]);
+    }
 }
