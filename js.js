dea8968a52348e437b0e5531399d73f125178a78
MichelleL8157 <98603675+MichelleL8157@users.noreply.github.com>
2023-01-17 01:01:29 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -2,25 +2,29 @@ let nameArr = ["Milk Carton", "Oreos", "Whipped Cream", "Cherios"];
 let priceArr = ["6.00", "4.50", "4.00", "2.50"];
 function addRow() {
     var newItem = document.getElementById("newItem").value;
-    var newPrice = document.getElementById("newPrice").value;
-    nameArr.push(newItem);
-    priceArr.push(newPrice);
-    var table = document.getElementById("table");
-    var tableLen = (table.rows.length);
-    var row = table.insertRow(tableLen).outerHTML="<tr id='row" + tableLen + "'><td id='itemRow" + tableLen + "'>" + newItem + "</td><td id='priceRow" + tableLen + "'>" + newPrice + "</td><td><input type='button' value='Delete' class='delete' onclick='deleteRow(" + tableLen + ")'></td></tr>";
-    document.getElementById("newItem").value="";
-    document.getElementById("newPrice").value="";
-    //to review the new list check the console:
-    console.log(nameArr);
-    console.log(priceArr);
+    let isNew = true;
+    for (let i = 0; i < nameArr.length; i++) {
+        let itemList = nameArr[i];
+        if (itemList === newItem) {
+            isNew = false;
+            break;
+        }
+    }
+    if (isNew) {
+        var newPrice = document.getElementById("newPrice").value;
+        nameArr.push(newItem);
+        priceArr.push(newPrice);
+        var table = document.getElementById("table");
+        var tableLen = (table.rows.length);
+        var row = table.insertRow(tableLen).outerHTML="<tr id='row" + tableLen + "'><td id='itemRow" + tableLen + "'>" + newItem + "</td><td id='priceRow" + tableLen + "'>" + newPrice + "</td><td><input type='button' value='Delete' class='delete' onclick='deleteRow(" + tableLen + ")'></td></tr>";
+        document.getElementById("newItem").value="";
+        document.getElementById("newPrice").value="";
+    }
 }
 function deleteRow(num) {
     document.getElementById("row" + num + "").outerHTML="";
     nameArr.splice(num - 1, 1);
     priceArr.splice(num - 1, 1);
-    //to review the new list check the console:
-    console.log(nameArr);
-    console.log(priceArr);
 }
 function change(direction) {
     let index = document.getElementById("switchRow").value;
